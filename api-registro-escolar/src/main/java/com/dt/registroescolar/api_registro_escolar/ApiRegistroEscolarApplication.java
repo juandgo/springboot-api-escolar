28ac8dfea2a4dcd17094f6af811e64cca2964b5d
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2025-07-04 17:39:05 -0500

    firt commit

---
+++ api-registro-escolar/src/main/java/com/dt/registroescolar/api_registro_escolar/ApiRegistroEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.dt.registroescolar.api_registro_escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRegistroEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRegistroEscolarApplication.class, args);
+	}
+
+}
---
+++ api-registro-escolar/src/test/java/com/dt/registroescolar/api_registro_escolar/ApiRegistroEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dt.registroescolar.api_registro_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRegistroEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
