4c93d4ee524ea0e27b09252be4eafbe6649ca950
Juan David Grijalba Osorio <55849409+juandgo@users.noreply.github.com>
2025-07-06 21:17:03 -0500

    Backup antes de actualizar Angular

--- registro-escolar-frontend/src/app/app.module.ts
+++ registro-escolar-frontend/src/app/app.module.ts
@@ -8,22 +8,23 @@ import {MatButtonModule} from '@angular/material/button';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
-import {MatInputModule} from '@angular/material/input';
+import { RegistroProfesorComponent } from './pages/registro-profesor/registro-profesor.component';
+import { RegistroEstudianteComponent } from './pages/registro-estudiante/registro-estudiante.component';
 
+import {MatInputModule} from '@angular/material/input';
 import { FormsModule } from '@angular/forms';
 import {MatSnackBarModule} from '@angular/material/snack-bar';
 import {MatCardModule} from '@angular/material/card';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import { RegistroProfesorComponent } from './pages/registro-profesor/registro-profesor.component';
-import { RegistroEstudianteComponent } from './pages/registro-estudiante/registro-estudiante.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     SignupComponent,
+    LoginComponent,
     RegistroProfesorComponent,
     RegistroEstudianteComponent,
   ],
